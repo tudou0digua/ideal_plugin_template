06481652705294138dee74d203856286edef316b
GitHub Action <action@github.com>
2023-12-24 02:46:27 +0000

    Template cleanup

--- src/main/kotlin/com/github/tudou0digua/idealplugintemplate/MyBundle.kt
+++ src/main/kotlin/com/github/tudou0digua/idealplugintemplate/MyBundle.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.plugins.template
+package com.github.tudou0digua.idealplugintemplate
 
 import com.intellij.DynamicBundle
 import org.jetbrains.annotations.NonNls
--- src/main/kotlin/com/github/tudou0digua/idealplugintemplate/toolWindow/MyToolWindowFactory.kt
+++ src/main/kotlin/com/github/tudou0digua/idealplugintemplate/toolWindow/MyToolWindowFactory.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.plugins.template.toolWindow
+package com.github.tudou0digua.idealplugintemplate.toolWindow
 
 import com.intellij.openapi.components.service
 import com.intellij.openapi.diagnostic.thisLogger
@@ -8,8 +8,8 @@ import com.intellij.openapi.wm.ToolWindowFactory
 import com.intellij.ui.components.JBLabel
 import com.intellij.ui.components.JBPanel
 import com.intellij.ui.content.ContentFactory
-import org.jetbrains.plugins.template.MyBundle
-import org.jetbrains.plugins.template.services.MyProjectService
+import com.github.tudou0digua.idealplugintemplate.MyBundle
+import com.github.tudou0digua.idealplugintemplate.services.MyProjectService
 import javax.swing.JButton
 
 
